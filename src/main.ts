cf4609ab9aa928d8fef289eb786133ff6a289b1f
anatolii <anatolii@anatoliis-MacBook-Air.local>
2023-04-25 02:32:29 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
